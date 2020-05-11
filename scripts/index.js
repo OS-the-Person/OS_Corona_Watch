2af83fcce66855a89bc40b59f50f47a91334f72a
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2020-05-11 15:21:54 +0100

    added daily numbers

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,3 @@
-async function getData() {
-    let api_url = "https://covid19.mathdro.id/api";
-    let response = await fetch(api_url);
-    let data = await response.json();
-    let { deaths, confirmed, recovered, lastUpdate  } = data;
-
-    deaths = deaths.value;
-    confirmed = confirmed.value;
-    recovered = recovered.value;
-    lastUpdate = lastUpdate.slice(0, 10)
-
-    document.getElementById("osConfirmedNo").textContent = confirmed;
-    document.getElementById("osDeathNo").textContent = deaths;
-    document.getElementById("osRecoveredNo").textContent = recovered;
-    document.getElementById("os-time").textContent = lastUpdate;
-}
-getData()
-
 
 async function searchCountry(country) {
     if (true){
@@ -33,9 +15,6 @@ async function searchCountry(country) {
         document.getElementById("osCountryDeath").textContent = deaths;
         document.getElementById("osCountryRecovered").textContent = recovered;
         document.getElementById("os-time").textContent = lastUpdate;
-    }
-    else {
-        console.log("Country don't exist")
     }
 }
 
--- scripts/onload_fxns.js
+++ scripts/onload_fxns.js
@@ -1,3 +1,4 @@
+//functiions that are called on page load
 async function getCountry() {
     let api_url = "https://covid19.mathdro.id/api/countries";
     let response = await fetch(api_url);
@@ -12,6 +13,24 @@ async function getCountry() {
 	};
 }
 getCountry();
+
+async function getData() {
+    let api_url = "https://covid19.mathdro.id/api";
+    let response = await fetch(api_url);
+    let data = await response.json();
+    let { deaths, confirmed, recovered, lastUpdate  } = data;
+
+    deaths = deaths.value;
+    confirmed = confirmed.value;
+    recovered = recovered.value;
+    lastUpdate = lastUpdate.slice(0, 10)
+
+    document.getElementById("osConfirmedNo").textContent = confirmed;
+    document.getElementById("osDeathNo").textContent = deaths;
+    document.getElementById("osRecoveredNo").textContent = recovered;
+    document.getElementById("os-time").textContent = lastUpdate;
+}
+getData()
 
 async function listDeaths() {
 	let api_url = "https://covid19.mathdro.id/api/deaths";
@@ -30,18 +49,17 @@ async function listDeaths() {
 			countries[vall] = x.deaths;
 		}
 	};
+
+	// this gets the countries object and outs the (key, value) into a list of tuples
 	let deathsArr = []
-	
-	console.log(countries)
-	
 	for (i in countries){
 		deathsArr.push([i, countries[i]]);
 	}
+	//this sorts the list of tuples from highest to lowest
 	deathsArr.sort((a, b) => {
 		return b[1] - a[1]
 	});
 	deathsArr.splice(10);
-	console.log(deathsArr);
 
 	var list = document.getElementById('osMaxDeath');
 	//clear element childs
@@ -63,4 +81,40 @@ async function listDeaths() {
 	};
 }
 listDeaths()
+
+async function dailyInfo() {
+	let api_url = "https://covid19.mathdro.id/api/daily";
+	let response = await fetch(api_url);
+	let data = await response.json();
 	
+	data = data.pop();
+	confirmed = data["deltaConfirmed"];
+	recovered = data["deltaRecovered"];
+	document.getElementById("dailyConfrmed").textContent = confirmed;
+    document.getElementById("dailyRecovered").textContent = recovered;
+	
+	let d = new Date()
+	let osDate = String(d.getMonth() + 1) + "-" + String((d.getDate()) - 1) + "-" + String(d.getFullYear())
+
+	response = await fetch(api_url + "/" + osDate);
+	data = await response.json();
+
+	let totalDeaths = 0;
+	for (i in data){
+		totalDeaths += Number(data[i]["deaths"])
+	}
+	
+	osDate = String(d.getMonth() + 1) + "-" + String((d.getDate()) - 2) + "-" + String(d.getFullYear())
+
+	response = await fetch(api_url + "/" + osDate);
+	data = await response.json();
+
+	for (i in data){
+		totalDeaths -= Number(data[i]["deaths"])
+	}
+
+	document.getElementById("dailyDeath").textContent = totalDeaths;   
+	
+
+}
+dailyInfo()
